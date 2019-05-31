54fe1bb78587a67d01eea36d7f901a5626e2ece0
edgar <edgarernestocruzreyes@gmail.com>
2019-05-31 13:49:04 -0500

    prueba servidor

---
+++ app.js
@@ -0,0 +1,24 @@
+const express =require ('express');
+const path = require('path');
+const bodyParser= require('body-parser')
+const app = express();
+const routes = require('./routes/index');
+// configuracion
+app.set('port', process.env.PORT|| 3000);// pide puerto al sistema
+app.set('views', path.join( __dirname,'views'));
+app.set('view engine','ejs');
+
+// middlewares
+app.use((req, res, next) =>{
+  console.log('$(req.url) -$(req.method)');
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// routes
+app.use(routes);
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+// iniciar servidor
+app.listen(app.get('port'),()=> {console.log('Server on port', app.get('port'))});
